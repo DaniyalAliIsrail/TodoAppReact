8177857d9eb87b181ecd1f3e7bf92f9f6516369a
Daniyal ali <daniyalali12568@gmail.com>
2023-09-10 05:22:40 +0500

    todocomplet

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,58 @@ import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Button from "./components/Button/Button";
+import { InputDefault } from "./components/inputbarshowui";
 
 function App() {
   const [todoElement, settodoElements] = useState("");
   const [uiShow, setuiShow] = useState([]);
-
+  let [uiinput, setuiInput] = useState("");
+  // ******************** AddTodo function ****************************************
   const addTodo = () => {
-    if (todoElement !== "") {
-      uiShow.push(todoElement);
-      setuiShow([...uiShow]);
-      
-    } else {
+    let objTodo = {
+      isEdit: false,
+      value: todoElement,
+    };
+    if (!objTodo.value) {
       alert("fill input filled");
+      return;
     }
+    uiShow.push(objTodo);
+    setuiShow([...uiShow]);
+    settodoElements("");
   };
+
+  console.log(todoElement);
   console.log(uiShow);
-  const delAll = () => {    
+  // ******************** DeleteAll function ****************************************
+  const delAll = () => {
     setuiShow([]);
   };
-  const delTodo = (ind) => {
-    uiShow.splice(ind, 1);
+  // ******************** DeleteTodo function *********************
+
+  const delTodo = (index) => {
+    uiShow.splice(index, 1);
     setuiShow([...uiShow]);
   };
+  // ******************** EditTodo function ****************************************
 
   const editTodo = (index) => {
-    uiShow[index] = prompt("Edit Value");
+    uiShow.forEach((element) => {
+      console.log(element);
+      element.isEdit = false;
+    });
+    uiShow[index].isEdit = true;
     setuiShow([...uiShow]);
+    setuiInput(uiShow[index].value); //setuiinput 3rd state hay or iskay kay andr first vlaue savehay hay js may nay bilal lkh kr addtodo keya
   };
+  const edit_todo_element = (index) => {
+    uiShow[index].isEdit = false; //Object kay andr jo isedit name k kay hay usay false kardo
+    uiShow[index].value = uiinput;  //Object kay andr jo value name k key hay usay default value pr save krado ku kay user nay kch changes nhy kyea or na edit
+    setuiShow([...uiShow]);
+    uiinput[index] = uiShow[index].value;
+    setuiInput([...uiinput]);
+  };
+
 
   return (
     <>
@@ -37,17 +62,17 @@ function App() {
       </section>
 
       {/* Input-Bar */}
-
       <div className=" flex justify-center  items-center mt-5 flex-col  ">
         <div className="w-[70%]">
           <input
             type="text"
             placeholder="ENTER YOUR NAME"
-            className="border w-full rounded-lg p-3 outline-none border-gray-600 focus:ring-purple-900 "
+            className="border w-full rounded-lg p-3 outline-none border-purple-600 focus:ring-purple-900 "
             id="todoInput"
             onChange={(e) => {
               settodoElements(e.target.value);
             }}
+            value={todoElement}
           />
         </div>
         <div className=" flex gap-3 mt-5 ">
@@ -56,21 +81,63 @@ function App() {
         </div>
       </div>
       {/* Todos item */}
-
       <div>
         {uiShow.map((value, index) => {
           return (
-            <div className="  w-[70%] mx-auto mt-5 ">
-              <ul key={index} className="">
-                <li className="text-xl p-3 border rounded-lg flex justify-between items-center">
-                  {value}
-                  <div className="flex">
-                    <Button trigger={() => editTodo(index)} title="EDIT" />
-                    <Button trigger={() => delTodo(index)} title="Delete" />
-                  </div>
-                </li>
-              </ul>
-            </div>
+            <section key={index}>
+              {value.isEdit ? (
+                <>
+                  {" "}
+                  <section className="w-[70%] mx-auto mt-5">
+                    <ul>
+                      <li className="text-xl p-3 border rounded-lg flex justify-between items-center">
+                        <input
+                          type="text"
+                          className="w-full border border-none outline-none p-2"
+                          onChange={(e) => {
+                            setuiInput(e.target.value);
+                          }}
+                          value={uiinput}
+                        ></input>
+                        <div className="flex">
+                          <Button
+                            trigger={() => {
+                              edit_todo_element(index);
+                            }}
+                            title="save"
+                          />
+                          <Button
+                            trigger={() => delTodo(index)}
+                            title="complet"
+                          />
+                        </div>
+                      </li>
+                    </ul>
+                  </section>
+                </>
+              ) : (
+                <>
+                  {" "}
+                  <section className="w-[70%] mx-auto mt-5">
+                    <ul>
+                      <li className="text-xl p-3 border rounded-lg flex justify-between items-center">
+                        {value.value}
+                        <div className="flex">
+                          <Button
+                            trigger={() => editTodo(index)}
+                            title="EDIT"
+                          />
+                          <Button
+                            trigger={() => delTodo(index)}
+                            title="Delete"
+                          />
+                        </div>
+                      </li>
+                    </ul>
+                  </section>
+                </>
+              )}
+            </section>
           );
         })}
       </div>
---
+++ src/components/inputbarshowui.jsx
@@ -0,0 +1,10 @@
+
+import { Input } from "@material-tailwind/react";
+ 
+export function InputDefault() {
+  return (
+    <div className="w-72">
+      <Input label="Username" />
+    </div>
+  );
+}
